463a63aa4a6884cf46018ee5a55c5d104026dec1
onetwothreeefour <32172906@dankook.ac.kr>
2020-01-11 21:48:39 +0900

    Appen something

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 url = ('https://www.premierleague.com/tables')
 
 rank = requests.get(url)
-print(rank.content)
 soup = BeautifulSoup(rank.content,'html.parser')
 
-team_rank_wds = soup.select('tbody>tr')
+team_rank = soup.select('tbody>tr')
+team_rank_wds = soup.find_all('td')
 
-for team_rank_wds in range(0,40,2):
+for num in range (1,40,2):
+    a=team_rank[num]
+    for num2 in range (2,11):
+        print(team_rank_wds[num2])[num]
